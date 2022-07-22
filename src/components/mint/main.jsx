69c451980c4a68a834467a21ef3ad4f351d2b8bc
Passionist59 <hiramjake59@gamil.com>
2022-07-21 21:01:58 -0400

    integration complete

--- src/components/mint/main.jsx
+++ src/components/mint/main.jsx
@@ -12,17 +12,6 @@ window.Buffer = Buffer;
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require('keccak256');
 
-const preAddresses = contract.premint;
-console.log("pre addresses", preAddresses);
-  const preleafNodes = preAddresses.map(addr => keccak256(addr));
-  console.log("pre leaf node", preleafNodes);
-  const premerkleTree = new MerkleTree(preleafNodes, keccak256, { sortPairs: true });
-  console.log("per merkleTree", premerkleTree);
-  var prehash = premerkleTree.getHexRoot();
-  console.log("prehash",prehash);
-  var proof = premerkleTree.getHexProof(keccak256("0xF170dCd31C71d7008F5eAaB197b03c4DE10ecB9d"));
-  console.log("preproof sdfsdf", proof);
-
 const Main = () => {
   const [inputValue, setInputValue] = useState(1);
   const [chainId, setChainId] = useState();
@@ -57,25 +46,11 @@ const Main = () => {
     setSigner(signer_metamask);
     const currentAccount = await signer_metamask.getAddress()
     setWalletAddress(currentAccount);
-    console.log("current account", currentAccount);
     
     const { chainId } = await provider.getNetwork();
     
     setChainId(chainId);
     setWalletConnected(true);
-
-    const preAddresses = contract.premint;
-    const preleafNodes = preAddresses.map(addr => keccak256(addr));
-    const premerkleTree = new MerkleTree(preleafNodes, keccak256, { sortPairs: true });
-    var preProof = premerkleTree.getHexProof(keccak256("0xF170dCd31C71d7008F5eAaB197b03c4DE10ecB9d"));
-    setPreProof(preProof);
-    console.log("preproof sdfsdf", preProof);
-
-    const whitelistAddresses = contract.whitelist;
-    const whitelistleafNodes = whitelistAddresses.map(addr => keccak256(addr));
-    const whitelistmerkleTree = new MerkleTree(whitelistleafNodes, keccak256, { sortPairs: true });
-    setWhiteProof(whitelistmerkleTree.getHexProof(keccak256(currentAccount)));
-    console.log("white proof", whitelistmerkleTree.getHexProof(keccak256(currentAccount)));
   }
 
   const createNFT = async () => {
@@ -109,57 +84,20 @@ const Main = () => {
       });
       return;
     } else if(step == 1){
-      console.log("Pre proof", preProof);
-      // try{
-      //   await nftContract.preMint(inputValue, proofAddress);
-      //   // nftContract.on("mint", (address, event) => {
-      //   //   toast.success('Success Full mint!', {
-      //   //     position: "top-right",
-      //   //     autoClose: 3000,
-      //   //     closeOnClick: true,
-      //   //     hideProgressBar: true,
-      //   //   });
-      //   // });
-      // } catch(error){
-      //   if (error["code"] === 4001) {
-      //     toast.error(error["message"].split(":")[1], {
-      //       position: "top-right",
-      //       autoClose: 3000,
-      //       hideProgressBar: false,
-      //       closeOnClick: true,
-      //       pauseOnHover: true,
-      //       draggable: true,
-      //       progress: 0,
-      //       theme: "dark",
-      //       transition: Flip,
-      //     });
-      //   } else {
-      //     console.log("Error",error["message"].split(":")[16].split('"')[0]);
-      //     toast.error(error["message"].split(":")[16].split('"')[0], {
-      //       position: "top-right",
-      //       autoClose: 3000,
-      //       hideProgressBar: false,
-      //       closeOnClick: true,
-      //       pauseOnHover: true,
-      //       draggable: true,
-      //       progress: 0,
-      //       theme: "dark",
-      //       transition: Flip,
-      //     });
-      //   }
-      // }
-    } else if(step == 2){
-
+      const preAddresses = contract.premint;
+      const preleafNodes = preAddresses.map(addr => keccak256(addr));
+      const premerkleTree = new MerkleTree(preleafNodes, keccak256, { sortPairs: true });
+      var preProof = premerkleTree.getHexProof(keccak256(walletAddress));
       try{
-        await nftContract.whitelistMint(inputValue, );
-        // nftContract.on("mint", (address, event) => {
-        //   toast.success('Success Full mint!', {
-        //     position: "top-right",
-        //     autoClose: 3000,
-        //     closeOnClick: true,
-        //     hideProgressBar: true,
-        //   });
-        // });
+        await nftContract.preMint(inputValue, preProof);
+        nftContract.on("mint", (address, event) => {
+          toast.success('Success Full mint!', {
+            position: "top-right",
+            autoClose: 3000,
+            closeOnClick: true,
+            hideProgressBar: true,
+          });
+        });
       } catch(error){
         if (error["code"] === 4001) {
           toast.error(error["message"].split(":")[1], {
@@ -188,6 +126,96 @@ const Main = () => {
           });
         }
       }
+    } else if(step == 2){
+      const whitelistAddresses = contract.whitelist;
+      const whitelistleafNodes = whitelistAddresses.map(addr => keccak256(addr));
+      const whitelistmerkleTree = new MerkleTree(whitelistleafNodes, keccak256, { sortPairs: true });
+      var whitelistproof = whitelistmerkleTree.getHexProof(keccak256(walletAddress));
+      try{
+        await nftContract.whitelistMint(inputValue, whitelistproof);
+        nftContract.on("mint", (address, event) => {
+          toast.success('Success Full mint!', {
+            position: "top-right",
+            autoClose: 3000,
+            closeOnClick: true,
+            hideProgressBar: true,
+          });
+        });
+      } catch(error){
+        if (error["code"] === 4001) {
+          toast.error(error["message"].split(":")[1], {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: 0,
+            theme: "dark",
+            transition: Flip,
+          });
+        } else {
+          console.log("Error",error["message"].split(":")[16].split('"')[0]);
+          toast.error(error["message"].split(":")[16].split('"')[0], {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: 0,
+            theme: "dark",
+            transition: Flip,
+          });
+        }
+      }
+    } else if(step == 3){
+      try{
+        await nftContract.publicSaleMint(inputValue);
+        nftContract.on("mint", (address, event) => {
+          toast.success('Success Full mint!', {
+            position: "top-right",
+            autoClose: 3000,
+            closeOnClick: true,
+            hideProgressBar: true,
+          });
+        });
+      } catch(error){
+        if (error["code"] === 4001) {
+          toast.error(error["message"].split(":")[1], {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: 0,
+            theme: "dark",
+            transition: Flip,
+          });
+        } else {
+          console.log("Error",error["message"].split(":")[16].split('"')[0]);
+          toast.error(error["message"].split(":")[16].split('"')[0], {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: 0,
+            theme: "dark",
+            transition: Flip,
+          });
+        }
+      }
+    } else if(step == 4){
+      toast.success('KINGS has already finished now!', {
+        position: "top-right",
+        autoClose: 3000,
+        closeOnClick: true,
+        hideProgressBar: true,
+        theme: "dark",
+      });
     }
   }
 
@@ -229,7 +257,7 @@ const Main = () => {
                 </span>
               </Link>
               :
-              <Link disable to="" className="Wallet" onClick={() => connectWallet()}>
+              <Link disable to="" className="Wallet" onClick={() => createNFT()}>
                 <span>
                   Mint
                   <div className="yl_bg">
